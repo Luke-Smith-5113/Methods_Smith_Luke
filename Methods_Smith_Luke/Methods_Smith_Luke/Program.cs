02a025407fe914f3a8c15c98b5d49ac78376e353
Luke-Smith-5113 <114005746+Luke-Smith-5113@users.noreply.github.com>
2023-03-30 09:29:11 -0500

    Add files via upload

---
+++ Methods_Smith_Luke/Methods_Smith_Luke/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Methods_Smith_Luke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //asks for two ints to multiply and saves them
+            Console.WriteLine("input an int to multiply");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("input another");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            //prints the product of the two ints
+            Console.Write("the product of those is ");
+            Console.WriteLine(multMeth(x, y));
+
+            //asks for a name and saves it as a string
+            Console.WriteLine("input your name");
+            string name = Console.ReadLine();
+
+            //prints a greeting
+            Console.WriteLine(stringMeth(name));
+        }
+        static int multMeth(int x, int y)
+        {
+            //multiplies ints x and y then returns the product
+            return (x * y);
+        }
+        static string stringMeth(string name)
+        {
+            //writes the greeting then returns it
+            return $"Hello {name}! It is nice to meet you!";
+        }
+    }
+}
